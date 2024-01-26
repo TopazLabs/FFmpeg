e3a2963b8889f92f3e1db1c247aa7a957a31ea00
Arpit <arpit.agrawal@topazlabs.com>
2024-01-26 16:52:07 -0600

    Add wrapper script for msys2

---
+++ build-scripts/win/build_win.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+cd "$(Build.SourcesDirectory)/FFmpeg"
+source ./build-scripts/win/setup-msvc-toolchain.sh
+./configure --toolchain=msvc --prefix=output-conan --enable-libvpx --enable-libaom --enable-shared --enable-x86asm --x86asmexe=yasm --enable-nvenc --enable-nvdec --disable-vulkan --enable-amf --enable-libvpl --enable-zlib --enable-tvai --extra-cflags="-I./conan/lib3rdparty/videoai/include -I./conan/lib3rdparty/amf/include -I./conan/lib3rdparty/nv-codec-headers/include -I./conan/lib3rdparty/libvpx/include -I./conan/lib3rdparty/aom/include -I./conan/lib3rdparty/libvpl/include/vpl" --extra-ldflags="-libpath:./conan/lib3rdparty/videoai/lib -libpath:./conan/lib3rdparty/zlib/lib -libpath:./conan/lib3rdparty/libvpx/lib -libpath:./conan/lib3rdparty/aom/lib -libpath:./conan/lib3rdparty/libvpl/lib -incremental:no"
+make clean
+make -r -j$(nproc) install
