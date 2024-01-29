640f199a5827e179765e5bb1c0f958b8a7fd736b
Arpit <arpit.agrawal@topazlabs.com>
2024-01-29 14:00:20 -0600

    Use cygwin for Windows paths

--- build-scripts/win/build_win.sh
+++ build-scripts/win/build_win.sh
@@ -5,8 +5,8 @@ set -e
 # Navigate to sources directory
 cd "$(dirname "$0")/../../"
 source ./build-scripts/win/setup-msvc-toolchain.sh
-echo $PATH
-echo $PKG_CONFIG_PATH
+echo "$PATH"
+echo "$PKG_CONFIG_PATH"
 ./configure --toolchain=msvc --prefix=output-conan --enable-libvpx --enable-libaom --enable-shared --enable-x86asm --x86asmexe=yasm --enable-nvenc --enable-nvdec --disable-vulkan --enable-amf --enable-libvpl --enable-zlib --enable-tvai --extra-cflags="-I./conan/lib3rdparty/videoai/include -I./conan/lib3rdparty/amf/include -I./conan/lib3rdparty/nv-codec-headers/include -I./conan/lib3rdparty/libvpx/include -I./conan/lib3rdparty/aom/include -I./conan/lib3rdparty/libvpl/include/vpl" --extra-ldflags="-libpath:./conan/lib3rdparty/videoai/lib -libpath:./conan/lib3rdparty/zlib/lib -libpath:./conan/lib3rdparty/libvpx/lib -libpath:./conan/lib3rdparty/aom/lib -libpath:./conan/lib3rdparty/libvpl/lib -incremental:no"
 make clean
 make -r -j$(nproc) install
--- build-scripts/win/setup-msvc-toolchain.sh
+++ build-scripts/win/setup-msvc-toolchain.sh
@@ -2,19 +2,25 @@
 
 if [[ -z "$CUDA_PATH" ]]; then
 	echo "CUDA_PATH is not set. nvenc will not be available."
-elif [[ -z "$VCINSTALLDIR" ]]; then
+fi
+if [[ -z "$VCINSTALLDIR" ]]; then
 	echo "Couldn't find Visual Studio install location. Aborting."
 	return 1
-elif [[ -z "$WindowsSdkVerBinPath" ]]; then
+fi
+if [[ -z "$WindowsSdkVerBinPath" ]]; then
 	echo "WindowsSdkVerBinPath is not set. Aborting."
 	return 1
 fi
 
-export PATH="${VCINSTALLDIR}/Tools/MSVC/${VCToolsVersion}/bin/Hostx64/x64/":$PATH
-export PATH="${VCINSTALLDIR}/../MSBuild/Current/Bin":$PATH
-if [[ ! -z "$CUDA_PATH" ]]; then
-	export PATH="${CUDA_PATH}/bin/":$PATH
-fi
-export PATH="${WindowsSdkVerBinPath}/x64/":$PATH
-export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig":$PKG_CONFIG_PATH
+# Convert Windows paths to Unix-style paths
+VCINSTALLDIR_UNIX=$(cygpath -u "$VCINSTALLDIR")
+WindowsSdkVerBinPath_UNIX=$(cygpath -u "$WindowsSdkVerBinPath")
+CUDA_PATH_UNIX=$(cygpath -u "$CUDA_PATH")
 
+export PATH="${VCINSTALLDIR_UNIX}/Tools/MSVC/${VCToolsVersion}/bin/Hostx64/x64/":$PATH
+export PATH="${VCINSTALLDIR_UNIX}/../MSBuild/Current/Bin":$PATH
+if [[ ! -z "$CUDA_PATH" ]]; then
+	export PATH="${CUDA_PATH_UNIX}/bin/":$PATH
+fi
+export PATH="${WindowsSdkVerBinPath_UNIX}/x64/":$PATH
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig":$PKG_CONFIG_PATH
