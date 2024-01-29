122df9f64f04bdf288a4c5e98b533f5954722db3
Arpit <arpit.agrawal@topazlabs.com>
2024-01-29 14:12:55 -0600

    Update script to better handle missing environment variables

--- build-scripts/win/setup-msvc-toolchain.sh
+++ build-scripts/win/setup-msvc-toolchain.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 if [[ -z "$CUDA_PATH" ]]; then
-	echo "CUDA_PATH is not set. nvenc will not be available."
+    echo "CUDA_PATH is not set. nvenc will not be available."
+else
+    CUDA_PATH_UNIX=$(cygpath -u "$CUDA_PATH")
+    export PATH="${CUDA_PATH_UNIX}/bin/":$PATH
 fi
+
 if [[ -z "$VCINSTALLDIR" ]]; then
-	echo "Couldn't find Visual Studio install location. Aborting."
-	return 1
+    echo "Couldn't find Visual Studio install location. Aborting."
+    return 1
+else
+    VCINSTALLDIR_UNIX=$(cygpath -u "$VCINSTALLDIR")
+    export PATH="${VCINSTALLDIR_UNIX}/Tools/MSVC/${VCToolsVersion}/bin/Hostx64/x64/":$PATH
+    export PATH="${VCINSTALLDIR_UNIX}/../MSBuild/Current/Bin":$PATH
 fi
+
 if [[ -z "$WindowsSdkVerBinPath" ]]; then
-	echo "WindowsSdkVerBinPath is not set. Aborting."
-	return 1
+    echo "WindowsSdkVerBinPath is not set. Aborting."
+    return 1
+else
+    WindowsSdkVerBinPath_UNIX=$(cygpath -u "$WindowsSdkVerBinPath")
+    export PATH="${WindowsSdkVerBinPath_UNIX}/x64/":$PATH
 fi
 
-# Convert Windows paths to Unix-style paths
-VCINSTALLDIR_UNIX=$(cygpath -u "$VCINSTALLDIR")
-WindowsSdkVerBinPath_UNIX=$(cygpath -u "$WindowsSdkVerBinPath")
-CUDA_PATH_UNIX=$(cygpath -u "$CUDA_PATH")
-
-export PATH="${VCINSTALLDIR_UNIX}/Tools/MSVC/${VCToolsVersion}/bin/Hostx64/x64/":$PATH
-export PATH="${VCINSTALLDIR_UNIX}/../MSBuild/Current/Bin":$PATH
-if [[ ! -z "$CUDA_PATH" ]]; then
-	export PATH="${CUDA_PATH_UNIX}/bin/":$PATH
-fi
-export PATH="${WindowsSdkVerBinPath_UNIX}/x64/":$PATH
 export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig":$PKG_CONFIG_PATH
