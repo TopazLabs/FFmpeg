d7033b17441a7d8933e0b5375d58316d6a7d8ebe
Mike Nguyen <mike.nguyen@topazlabs.com>
2026-08-31 14:27:33 -0500

    bumped videoai version

--- build-scripts/conanfile.py
+++ build-scripts/conanfile.py
@@ -28,7 +28,7 @@ class conanRecipe(ConanFile):
 
     # windows libaom-av1 build different recipe revision id for some reason...
     def requirements(self):
-        self.requires("videoai/2.0.39-vaio+3")
+        self.requires("videoai/2.0.39-vaio+5")
         self.requires("libvpx/1.14.1")
         self.requires("dav1d/1.5.3")
         if self.settings.os == "Macos" and self.settings.arch == "x86_64":
